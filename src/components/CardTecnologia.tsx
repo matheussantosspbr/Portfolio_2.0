a90f69fb08acacf00bba04db7674ae2f3f49d773
Matheus Santos <matheussantosspbr@gmail.com>
2023-02-02 22:48:27 -0300

    implementação do novo layout dos cards - com bug

--- src/components/CardTecnologia.tsx
+++ src/components/CardTecnologia.tsx
@@ -6,23 +6,44 @@ interface Props{
     alt:string;
   }
 
-export default function CardTecnologia({linguagem, arquivo,colorStroke,alt, colorShadow}: Props ){
+  export default function CardTecnologia({linguagem, arquivo,colorStroke,alt, colorShadow}: Props ){
+
+    
+    const cards = document.querySelectorAll('.card')
+
+    for (var i = 0; i < cards.length; i++) {
+        cards[i].addEventListener("click", function (e) {
+            this.classList.toggle("flip")
+    })
+}
+
+    // for (var i = 0; i < cards.length; i++) {
+    //     cards[i].addEventListener("click",  e => {
+    //       console.log(e)
+          // alert(e)
+    //     })
+    // }
+   
+    // cards.forEach((card) => {
+    //   card.addEventListener("click", active)
+    // })
+   
+    // function active(event:any) {
+    //   cards.forEach((card) => {
+    //    card.classList.remove("flip")
+      
+    //    if(card.contains(event.target)) {
+    //     card.classList.add("flip")
+    //    }
+    //   })
+    // }
 
 
     return(
-        <div className={`
-                border
-                w-60
-                h-60
-                rounded-lg
-                ml-5
-                mr-5
-                mb-8
-                border-transparent
-                shadow-center
-                ${colorShadow}
-        `}>
-            <div className="h-48">
+      <div className='!w-full h-108'> 
+        <div className="card">
+          <div className="face front">
+          <div className="h-48">
             <div className="relative top-5 left-10 w-full">
               <svg className="w-full">
                 <circle cx="75" cy="75" r="70" className="fill-none stroke-black stroke-5"></circle>
@@ -34,6 +55,12 @@ export default function CardTecnologia({linguagem, arquivo,colorStroke,alt, colo
             </figure>
             </div>
             <p className="text-white w-full text-center font-medium">{linguagem}</p>
+          </div>
+        <div className="face back">
+          back
         </div>
+        </div>
+      </div>
+       
     )
 }
